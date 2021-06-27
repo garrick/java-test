4e22bfdb1f4526252d9e99fa7a1a503e2e90f98a
Garrick West <garrick@acm.org>
2021-06-26 17:12:42 -0700

    Parameterizing HenrysGrocery test

--- app/src/test/java/org/commandline/java/test/HenrysGroceryTest.java
+++ app/src/test/java/org/commandline/java/test/HenrysGroceryTest.java
@@ -1,17 +1,20 @@
 package org.commandline.java.test;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class HenrysGroceryTest {
 
-    @Test
-    public void testAllAvailableStockItemsByName() {
+    @ParameterizedTest
+    @ValueSource(strings = {"soup"})
+    public void testAllAvailableStockItemsByName(String itemName) {
         //Arrange
         HenrysGrocery unit = new HenrysGrocery();
         //Act
-        StockItem stockItem = unit.getStockItemByName("soup");
+        StockItem stockItem = unit.getStockItemByName(itemName);
         //Assert
         assertNotNull(stockItem);
     }
