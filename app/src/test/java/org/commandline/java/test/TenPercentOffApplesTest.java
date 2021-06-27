905f58cdb1ee97b1e96c8ee1a3dee19a5c519252
Garrick West <garrick@acm.org>
2021-06-26 22:54:48 -0700

    Adding one day past 10% apples discount test.

--- app/src/test/java/org/commandline/java/test/TenPercentOffApplesTest.java
+++ app/src/test/java/org/commandline/java/test/TenPercentOffApplesTest.java
@@ -3,6 +3,8 @@ package org.commandline.java.test;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -27,6 +29,19 @@ public class TenPercentOffApplesTest {
         TenPercentOffApples unit = new TenPercentOffApples(henrysGrocery);
         //Act
         boolean isAvailable = unit.isCurrentlyAvailable(LocalDateTime.now().plusDays(2));
+        //Assert
+        assertFalse(isAvailable);
+    }
+
+    @Test
+    public void testIsCurrentlyAvailableIsFalseTooLate() {
+        //Arrange
+        HenrysGrocery henrysGrocery = new HenrysGrocery();
+        TenPercentOffApples unit = new TenPercentOffApples(henrysGrocery);
+        //Act
+        LocalDateTime startOfThisMonth = LocalDateTime.now().minusDays(LocalDateTime.now().getDayOfMonth());
+        LocalDateTime endOfFollowingMonthPlusOneDay = startOfThisMonth.plusMonths(2).plusDays(1);
+        boolean isAvailable = unit.isCurrentlyAvailable(endOfFollowingMonthPlusOneDay);
         //Assert
         assertFalse(isAvailable);
     }
