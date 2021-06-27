31e239c4acbddc1ec6ef8dd05498f94df0f7d76d
Garrick West <garrick@acm.org>
2021-06-26 18:13:09 -0700

    Clarifying IllegalArgumentException w/ description for Product constructor.

--- app/src/main/java/org/commandline/java/test/Product.java
+++ app/src/main/java/org/commandline/java/test/Product.java
@@ -4,7 +4,7 @@ public class Product {
     private final String name;
 
     public Product(String name) {
-        if (name == null || "".equals(name)) throw new IllegalArgumentException();
+        if (name == null || "".equals(name)) throw new IllegalArgumentException("Null and blank Product names not allowed");
         this.name = name;
     }
 
