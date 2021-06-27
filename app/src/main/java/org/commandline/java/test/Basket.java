f88f9ce2e7776dbdaca798ed44377c70643b3a48
Garrick West <garrick@acm.org>
2021-06-26 18:21:16 -0700

    Added full implementation for adding item to Basket

--- app/src/main/java/org/commandline/java/test/Basket.java
+++ app/src/main/java/org/commandline/java/test/Basket.java
@@ -1,12 +1,15 @@
 package org.commandline.java.test;
 
+import java.util.ArrayList;
+
 public class Basket {
 
+    private ArrayList<StockItem> items = new ArrayList<>();
     public void add(StockItem stockItem) {
-
+        this.items.add(stockItem);
     }
 
     public int itemCount() {
-        return 2;
+        return items.size();
     }
 }
